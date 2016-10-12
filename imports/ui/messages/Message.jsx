a546ef6849b7c25751829c32dd270bb14ab9542e
nazar yavnyy <ny1am.lviv.ua@gmail.com>
2016-10-12 09:27:46 +0300

    revised message's initial type view

--- imports/ui/messages/Message.jsx
+++ imports/ui/messages/Message.jsx
@@ -7,14 +7,27 @@ export default function Message ({message, currentUser}) {
   const time = getTime(message.timestamp);
   const messageClass = currentUser._id == message.userId ? 'message-mine' : 'message-other';
 
-  return (
-    <div className={`message ${messageClass}`}>
-      <p className='message-text'>
-        {message.text}
-        <span className='message-timestamp'>{time}</span>
-      </p>
-    </div>
-  );
+  if (message.type === 'initial') {
+    return (
+      <div className='message-initial'>
+        <div className='message-initial-info'>
+          {time} {message.userId} created this conversation.
+        </div>
+        <p>
+          {message.text}
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className={`message ${messageClass}`}>
+        <p className='message-text'>
+          {message.text}
+          <span className='message-timestamp'>{time}</span>
+        </p>
+      </div>
+    );
+  }
 }
 
 Message.propTypes = {
